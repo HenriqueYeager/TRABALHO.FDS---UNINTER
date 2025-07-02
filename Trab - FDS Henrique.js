4ef7852f4314024935aeee40ed299c891c27571d
Douglas Henrique <doug-60@hotmail.com>
2025-07-02 18:41:35 -0300

    Add files via upload

---
+++ Trab - FDS Henrique.js
@@ -0,0 +1,6 @@
+
+function mostrarMensagem(){
+    const div = document.getElementById('mensagem');
+    div.innerText = "Olá Caro Viajante, O lance Não é Quanto Tempo Você Tem, é Como Você Usa!!"
+}
+
